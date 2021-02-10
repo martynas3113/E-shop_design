3dbdd0a62e26c35f16e3682c4e5b4a97815128b0
martynas3113 <martynas.pazusis@gmail.com>
2021-02-10 23:35:25 +0200

    Special price section and item filtering added

--- index.js
+++ index.js
@@ -17,4 +17,14 @@ $(document).ready(function(){
             }
         }
     });
+
+    var $grid = $(".grid").isotope({
+        itemSelector: ".grid-item",
+        layoutMode: "fitRows"
+    });
+
+    $(".button-group").on("click", "button",function(){
+        var filterValue = $(this).attr('data-filter');
+        $grid.isotope({filter:filterValue});
+    })
   });
