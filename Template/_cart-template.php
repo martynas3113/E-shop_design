1d493e5135468966a7b639c3a3ef4523bf882c16
martynas3113 <martynas.pazusis@gmail.com>
2021-02-19 20:42:12 +0200

    Show cart item count

--- Template/_cart-template.php
+++ Template/_cart-template.php
@@ -38,7 +38,7 @@
                         </div>
                         <div class="col-sm-2 text-end">
                             <div class="font-size-20 text-danger font-baloo">
-                                $<span class="product_price">152</span>
+                                $<span class="product_price"><?php echo $item['item_price'] ?? 0 ?></span>
                             </div>
                         </div>
 
@@ -52,7 +52,7 @@
                     <div class="sub-total text-center mt-2 border">
                         <h6 class=" font-size-12 text-success py-3"><i class="fas fa-check"></i>Your order is eligible for FREE Delivery.</h6>
                         <div class="border-top py-4">
-                            <h5 class="font-baloo font-size-20">Total (2 items) :&nbsp;<span class="text-danger">$<span class="text-danger" id="deal-price">152.00</span></span></h5>
+                            <h5 class="font-baloo font-size-20">Total (<?php echo count($product->getData('cart'))  ?> items) :&nbsp;<span class="text-danger">$<span class="text-danger" id="deal-price">152.00</span></span></h5>
                             <button type="submit" class="btn btn-warning py-2">Proceed to Buy</button>
                         </div>
                     </div>
